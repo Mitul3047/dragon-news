811ca533f45fe1c2f66172423e65e556d4874733
Shehab Ahamed Chowdhury Mitul <121333557+Mitul3047@users.noreply.github.com>
2023-10-03 09:53:12 +0600

    header section added

--- src/Pages/Home/BreakingNews/BreakingNews.jsx
+++ src/Pages/Home/BreakingNews/BreakingNews.jsx
@@ -1,10 +1,13 @@
-
+import Marquee from "react-fast-marquee";
 
 const BreakingNews = () => {
     return (
-        <div>
+        <div className="flex justify-center items-center">
             {/* https://www.react-fast-marquee.com/ */}
             <div className="btn btn-error text-white">Breaking News</div>
+            <Marquee pauseOnHover={true} speed={100}>
+                I can be a React component, multiple React components, or just some text.
+            </Marquee>
         </div>
     );
 };
